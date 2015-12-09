5bfb4f5824e4f58503d47d8d963ebbd22888fb15
kelechi.nwokonta <kelechi.nwokonta@td.com>
2015-12-08 23:07:47 -0500

    datasouce update working

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
  
     app.models.SpotRental.create([
       {FirstName: 'Kelechi', LastName: 'Nwokonta', Owner: 1, Renter: 0, Phone: '905-818-4475',Email: 'kcichad@hotmail.com', LotNumber: 46},
-      {FirstName: 'Sam', LastName: 'Phillips', Owner: 0, Renter: 1, Phone: '416-432-6578',Email: 'philsam@gmail.com', LotNumber: 46},
+      {FirstName: 'Sam', LastName: 'Phillips', Owner: 0, Renter: 1, Phone: '416-432-6578',Email: 'philsam@gmail.com', LotNumber: 7},
       {FirstName: 'Grace', LastName: 'Wang', Owner: 1, Renter: 1, Phone: '519-212-0073',Email: 'wang_g@gmail.com', LotNumber: 213},
     ], function(err, spotrentals) {
       if (err) throw err;
